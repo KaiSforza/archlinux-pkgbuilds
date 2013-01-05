f15ae01d9c33eecd1032abbd2232f405a6b1648a
William Giokas <1007380@gmail.com>
2013-01-05 02:07:41 -0600

    Edited dwm workspace tags
    changed some stuff for clang compilation too.
    
    Version bumps
    
    Signed-off-by: William Giokas <1007380@gmail.com>

--- vcs/dwm-git/config.h
+++ vcs/dwm-git/config.h
@@ -24,7 +24,7 @@ static const Bool topbar            = True;     /* False means bottom bar */
 /* static const Bool focusonwheelscroll = True;    False means scrollwheel doesn't change focus */
 
 /* tagging                    0    1    2    3    4    5    6    7    8  */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "_" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
