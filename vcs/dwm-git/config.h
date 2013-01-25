8d8964d6186f4685df602f7627b7f38b964a0a06
William Giokas <1007380@gmail.com>
2013-01-25 15:40:24 -0600

    General updates, some new packages
    new packages: mkinitcpio-git
                  netctl-git zsh completion file
    kernel update
    version bumps for the rest, mostly.
    
    Signed-off-by: William Giokas <1007380@gmail.com>

--- vcs/dwm-git/config.h
+++ vcs/dwm-git/config.h
@@ -63,8 +63,8 @@ static const Layout layouts[] = {
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
     { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-    { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-    { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+    { MODKEY|ShiftMask,             KEY,      toggleview,     {.ui = 1 << TAG} }, \
+    { MODKEY|ControlMask,           KEY,      tag,            {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
